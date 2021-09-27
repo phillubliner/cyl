a2c7b3ce503397826e4ba4e57ba73cd31caa170d
Carter <info@carterduong.com>
2021-09-27 15:11:47 -0700

    Base rotation done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
+import TWEEN from '@tweenjs/tween.js'
+import { ArrayCamera } from 'three'
+import { text } from 'body-parser'
 
 const container = document.querySelector('#three')
 
@@ -16,18 +19,34 @@ let images = [
   'static/3.png',
   'static/4.png',
   'static/5.png',
+  // 'static/kaje1.png',
+  // 'static/kaje2.jpg',
+  // 'static/kaje3.png',
+  // 'static/kaje4.jpg',
+  // 'static/kaje5.jpg',
 ]
 let textures = []
-let textureMetaData = []
+let textureWidths = []
+let textureHeights = []
+let minTextureHeight = 0
 let thetas = []
+let textureMidpoints = []
+let currentIndex = 0
+
+const carouselGroup = new THREE.Group()
+
+let tween 
 
 function createTextures() {
   for (let i = 0; i < images.length; i++) {
     textures[i] = new THREE.TextureLoader().load(images[i], (texture) => {
-      textureMetaData.push(texture.image.width)
+      textureWidths.push(texture.image.width)
+      textureHeights.push(texture.image.height)
 
-      const doneLoading = textureMetaData.length === images.length
+
+      const doneLoading = textureWidths.length === images.length
       if (doneLoading) {
+        minTextureHeight = Math.min.apply(null, textureHeights)
         kaje.init()
       }
     })
@@ -51,8 +70,8 @@ function init() {
   renderer.setClearColor(0x000000, 1.0)
 
   camera = new THREE.PerspectiveCamera(45, renderSize.x / renderSize.y, 1, 1000)
-  camera.position.y = 50
   camera.position.z = 100
+  camera.lookAt(new THREE.Vector3(0.0,0.0,0.0))
 
   container.appendChild(renderer.domElement)
 
@@ -64,12 +83,10 @@ function init() {
 init()
 
 function draw() {
-  rafId = requestAnimationFrame(draw)
   time += 0.01
-
+  rafId = requestAnimationFrame(draw)
   kaje.update()
-
-  camera.lookAt(new THREE.Vector3(0.0,0.0,0.0))
+  TWEEN.update()
   renderer.render(scene, camera, renderTarget)
 }
 
@@ -82,20 +99,13 @@ function draw() {
  */
 
 function KAJE(RENDERER, SCENE, CAMERA) {
-  const CELL_COUNT = 5.0
-
   this.renderer = RENDERER
   this.scene = SCENE
   this.camera = CAMERA
-  this.carousel = new THREE.Object3D()
-  this.segments = []
-	this.markers = []
-	this.mesh
-	this.textures = textures
-	this.FACEINDEX
-	this.FACEPORTION
 
+	this.textures = textures
   this.meshes = []
+  this.carouselGroup = carouselGroup
 
   this.init = function() {
     console.log('kaje init')
@@ -109,27 +119,43 @@ function KAJE(RENDERER, SCENE, CAMERA) {
     const gutterRadians = (gutter / 100) * TAU
 
     // calculate total width
-    for (let z = 0; z < textureMetaData.length; z++) {
-      totalWidth += textureMetaData[z]
+    for (let i = 0; i < textureWidths.length; i++) {
+      totalWidth += textureWidths[i]
     }
 
-    // calculate theta length for each image
-    for (let z = 0; z < textureMetaData.length; z++) {
-      const textureWidth = textureMetaData[z]
+    // calculate and store theta length for each image
+    for (let i = 0; i < textureWidths.length; i++) {
+      const textureWidth = textureWidths[i]
       const thetaLength = (textureWidth / totalWidth) * TAU + gutterRadians
       thetas.push(thetaLength)
+
+      // store texuture midpoints
+      // console.log('reduce', (textureWidth / 2) + textureWidths.slice(0, i + 1).reduce((a, b) => a + b))
+      let accumulatedWidth = 0
+
+      for (let j = 0; j < i; j++) {
+        accumulatedWidth += textureWidths[j]
+      }
+
+      console.log('accumulatedWidth', accumulatedWidth)
+      const midpoint = (textureWidth / 2) + accumulatedWidth
+      const midpointRadians = (midpoint / totalWidth) * TAU + gutterRadians
+      textureMidpoints.push(midpointRadians)
     }
 
+    console.log('widths', textureWidths)
     console.log('thetas', thetas)
+    console.log('midpoints', textureMidpoints)
 
     // create and rotate cylinder segments using calculated thetas
     for (let i = 0; i < textures.length; i++) {
-      console.log(textureMetaData[i], totalWidth, gutterRadians, thetas[i])
+      console.log(textureHeights[i] / minTextureHeight)
+      const relativeHeight = (textureHeights[i] / minTextureHeight) * 7 
 
-      let geo = new THREE.CylinderGeometry( 40, 40, 20, 32, 1, true, 0, thetas[i])
+      let geo = new THREE.CylinderGeometry( 40, 40, relativeHeight, 32, 1, true, 0, thetas[i])
       let mat = new THREE.MeshBasicMaterial({
         // wireframe: true,
-        side: THREE.DoubleSide,
+        // side: THREE.DoubleSide,
         color: Math.random() * 0xffffff,
         transparent: true,
         map: textures[i]
@@ -143,17 +169,68 @@ function KAJE(RENDERER, SCENE, CAMERA) {
       for (let j = 0; j < i; j++) {
         calculatedRotation += (thetas[j] + gutterRadians)
       }
-      console.log("calc'd rotation", calculatedRotation)
       mesh.rotation.y = calculatedRotation 
 
-      this.scene.add(mesh)
+      this.carouselGroup.add(mesh)
     }
+
+    this.carouselGroup.rotation.y -= textureMidpoints[0]
+    this.scene.add(this.carouselGroup)
   }
 
   this.update = function() {
-    for (let i = 0; i < this.meshes.length; i++) {
-      this.meshes[i].rotation.y += 0.003
-    }
   }
 
+}
+
+// Event Listeners
+document.querySelector('.controls .left').addEventListener('click', () => {
+  handleClick('left')
+})
+
+document.querySelector('.controls .right').addEventListener('click', () => {
+  handleClick('right')
+})
+
+function handleClick(dir) {
+  const prevIndex = currentIndex
+
+  if (dir === 'left') {
+    currentIndex = currentIndex === 0 ? textures.length - 1 : currentIndex - 1
+  } else {
+    currentIndex = currentIndex === (textures.length - 1) ? 0 : currentIndex + 1
+  }
+
+  // calculate angle difference
+  let oldPos = 0
+  let newPos = 0
+
+  oldPos = accumulateToIndex(textureWidths, prevIndex)
+  oldPos += textureWidths[prevIndex] / 2
+
+  newPos = accumulateToIndex(textureWidths, currentIndex)
+  newPos += textureWidths[currentIndex] / 2
+  console.log(prevIndex, oldPos, currentIndex, newPos)
+
+  const delta = newPos - oldPos
+  const deltaRadians = (delta / textureWidths.reduce((a, b) => a + b)) * TAU
+  console.log(deltaRadians)
+
+  const currRotation = carouselGroup.rotation
+
+  tween = new TWEEN.Tween(currRotation).to({
+    x: currRotation.x, y: currRotation.y - deltaRadians, z: currRotation.z 
+  }, 500);
+
+  tween.start()
+}
+
+function accumulateToIndex(arr, index) {
+  let result = 0
+
+  for (let i = 0; i < index; i++) {
+    result += arr[i]
+  }
+
+  return result
 }
